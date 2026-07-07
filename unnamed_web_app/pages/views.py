a4a6f0f4722bf6ea91584da5cf6f3906710b1eea
steven-benny <138088479+steven-benny@users.noreply.github.com>
2026-07-07 17:24:33 -0400

    Tweaked game detail page

--- unnamed_web_app/pages/views.py
+++ unnamed_web_app/pages/views.py
@@ -4,6 +4,11 @@ def home(request):
     return render(request, "pages/home.html")
 
 def game_detail(request):
+    selected_sort = request.GET.get("sort", "popular")
+    valid_sorts = {"popular", "new", "trending", "controversial"}
+    if selected_sort not in valid_sorts:
+        selected_sort = "popular"
+
     game = {
         "title": "Sekiro: Shadows Die Twice",
         "year": "2019",
@@ -16,25 +21,93 @@ def game_detail(request):
         "description": "A challenging action-adventure game focused on precision combat, exploration, and mastery.",
     }
 
-    recent_reviews = [
+    reviews = [
         {
             "user": "Alex",
+            "initial": "A",
             "rating": "5",
-            "text": "Brutal at first, but incredibly satisfying once the combat finally clicks.",
+            "text": "Brutal at first, but incredibly satisfying once the combat finally clicks. Every boss feels like a conversation you have to learn instead of a wall you have to break.",
+            "likes": "248",
+            "dislikes": "12",
+            "reply_count": "23",
+            "new_score": 2,
+            "trend_score": 86,
+            "controversy_score": 31,
+            "replies": [
+                {
+                    "user": "Nina",
+                    "initial": "N",
+                    "text": "blah blah blah",
+                },
+                {
+                    "user": "Owen",
+                    "initial": "O",
+                    "text": "blah blah blah",
+                },
+            ],
         },
         {
             "user": "Maya",
+            "initial": "M",
             "rating": "4.5",
             "text": "One of the cleanest combat systems I have ever played.",
+            "likes": "191",
+            "dislikes": "9",
+            "reply_count": "14",
+            "new_score": 3,
+            "trend_score": 94,
+            "controversy_score": 24,
+            "replies": [
+                {
+                    "user": "Jules",
+                    "initial": "J",
+                    "text": "blah blah blah",
+                },
+            ],
         },
         {
             "user": "Ryan",
             "rating": "2",
+            "initial": "R",
             "text": "This game sucks it's too hard.",
+            "likes": "42",
+            "dislikes": "118",
+            "reply_count": "37",
+            "new_score": 1,
+            "trend_score": 58,
+            "controversy_score": 96,
+            "replies": [
+                {
+                    "user": "Tessa",
+                    "initial": "T",
+                    "text": "blah blah blah",
+                },
+                {
+                    "user": "Cam",
+                    "initial": "C",
+                    "text": "blah blah blah",
+                },
+            ],
         },
+    ]
+
+    sorters = {
+        "popular": lambda review: int(review["likes"]) - int(review["dislikes"]),
+        "new": lambda review: review["new_score"],
+        "trending": lambda review: review["trend_score"],
+        "controversial": lambda review: review["controversy_score"],
+    }
+    reviews = sorted(reviews, key=sorters[selected_sort], reverse=True)
+
+    review_sort_options = [
+        {"value": "popular", "label": "Popular", "selected": selected_sort == "popular"},
+        {"value": "new", "label": "New", "selected": selected_sort == "new"},
+        {"value": "trending", "label": "Trending", "selected": selected_sort == "trending"},
+        {"value": "controversial", "label": "Controversial", "selected": selected_sort == "controversial"},
     ]
 
     return render(request, "pages/game_detail.html", {
         "game": game,
-        "recent_reviews": recent_reviews,
+        "reviews": reviews,
+        "review_sort_options": review_sort_options,
     })
